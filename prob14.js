3eee0cbc61bd1bcccda43871f6bf69d13edff2aa
Dibya Jyoti Borah <dibya.borah@taazaa.com>
2024-01-02 20:00:56 +0530

    uncommenting prob14 file

--- prob14.js
+++ prob14.js
@@ -2,17 +2,17 @@
 
 // const arr = [5,56,78,2,99];
 // const arr = [500,56,78,2,99];
-// const arr = [500,56,780,2,99];
+const arr = [500,56,780,2,99];
 
-// const maxNum = (anyArray) => {
-//     let num = anyArray[0];
-//     for(let i = 0; i<anyArray.length;i++){
-//         if (anyArray[i]> num) {
-//             num = anyArray[i];          
-//         }
-//     }
-//     return num;
-// }
+const maxNum = (anyArray) => {
+    let num = anyArray[0];
+    for(let i = 0; i<anyArray.length;i++){
+        if (anyArray[i]> num) {
+            num = anyArray[i];          
+        }
+    }
+    return num;
+}
 
-// const result = maxNum(arr);
-// console.log(result);
+const result = maxNum(arr);
+console.log(result);
